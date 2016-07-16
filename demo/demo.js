d93139e71bb7c42289cf730b6fa625b335ccffbb
Golf Sinteppadon <golf@twitch.tv>
2016-07-16 00:40:34 -0700

    commit the real website

---
+++ demo/demo.js
@@ -0,0 +1,83 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 15:46:25 Jun 16, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 7:17:26 Jul 16, 2016.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+Event.observe(window, "load", function() {
+	var frames = $$(".frame");
+	frames[0].style.display = "block";
+	
+	var hitboxes = $$(".hitbox, .nextButton");
+	for (var i = 0; i < hitboxes.length; i++) {
+		Event.observe(hitboxes[i], "click", nextFrame);
+	}
+	
+});
+
+function nextFrame(e) {
+	var currentFrame = getParentFrame(e.target);
+	var nextFrame = currentFrame.next();
+	
+	currentFrame.style.display = "none";
+	
+	if (nextFrame) {
+		nextFrame.style.display = "block";
+	}
+	fadeInElements(nextFrame);
+}
+
+function getParentFrame(element) {
+	while (!element.hasClassName("frame")) {
+		element = element.parentNode;
+	}
+	return element;
+}
+
+function fadeInElements(element) {
+	for (var i = 0; i < element.childElements().length; i++) {
+		if (navigator.appName !== "Microsoft Internet Explorer" && (
+			element.childElements()[i].hasClassName("description") || element.childElements()[i].hasClassName("hitbox"))) {
+			element.childElements()[i].style.opacity = "0";
+			new Effect.Appear(element.childElements()[i]);
+		}
+	}
+	
+	if (element.id == "1_start") {
+		updateTaskNumber(1);
+	}
+	if (element.id == "2_start") {
+		updateTaskNumber(2);
+	}
+	if (element.id == "3_start") {
+		updateTaskNumber(3);
+	}
+}
+
+function jumpToTask(taskNumber) {
+	var frames = $$(".frame");
+	for (var i = 0; i < frames.length; i++) {
+		frames[i].style.display = "none";
+	}
+	var newFrame = $(taskNumber + "_start");
+	newFrame.style.display = "block";
+	fadeInElements(newFrame);
+}
+
+function updateTaskNumber(taskNo) {
+	var buttons = $$(".jumpButtons button");
+	
+	for (var i = 0; i < buttons.length; i++) {
+		buttons[i].removeClassName("current");
+		if (i == taskNo - 1) {
+			buttons[i].addClassName("current");
+		}
+	}
+}
